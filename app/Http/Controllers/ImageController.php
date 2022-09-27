9b59b30e3af754618feeb9deccc4c69f815d05ba
Justinas Mickus <mickusjustinas1@gmail.com>
2022-09-27 18:46:35 +0200

    single large image

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -13,4 +13,8 @@ class ImageController extends Controller
 
         return view('image.index', compact('images'));
     }
+    public function show(Image $image)
+    {
+       return view('image.show', compact('image'));
+    }
 }
