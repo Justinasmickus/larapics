8a977be6daa9c0d04a23c68cf099ce9e38925b5e
Justinas Mickus <mickusjustinas1@gmail.com>
2022-10-06 15:44:49 +0200

    component default values and methods

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -5,13 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Blade Components</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
 </head>
 <body>
-    @php
+    {{-- @php
     $icon = "logo.svg"    
     @endphp
 
     <x-icon :src="$icon"/>
-    <x-ui.button/>
+    <x-ui.button/> --}}
+
+    <x-alert type="danger"/>
 </body>
 </html>
